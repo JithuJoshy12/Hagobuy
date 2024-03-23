02e9a0b27a99895ef55fe11b8042e72ddbb9ecd5
JithuJoshy12 <jithujoshy4431@gmail.com>
2024-03-23 19:13:08 -0400

    new update march 23

--- Hagobuy_Links/settings.py
+++ Hagobuy_Links/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i_echql(7x+yc_19ibg6^(zq%stbf$ei*k#pqhv$1r89d8_%rc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aabbyy.pythonanywhere.com']
 
 
 # Application definition
